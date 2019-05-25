98762a634bead96459b13823c37a418f370e36d8
Yasuhisa Kidokoro <detents20@gmail.com>
2019-05-25 12:15:43 +0900

    aaa

---
+++ src/step1/q-1-1/js/ms.js
@@ -0,0 +1,24 @@
+(function(){
+ loop1();
+ loop2();
+})();
+
+function loop1(){
+ var loop1 =[];
+ for (var i = 0; i < 10; i++){
+  loop1.push(i+'');
+ }
+ $('#loop1').text(loop1.join("));
+}
+
+function loop2(){
+ var loop2 =[];
+ for (var i = 0; i < 10; i++){
+  loop2.push('<p>');
+  for(var j = 0; j < 10; J++){
+   loop2.push('*');
+  }
+  loop2.push('</p>');
+ }
+ $('#loop2').html(loop2.join("));
+}
